badc9c3953bceeaa5a1519087b872a5b29772544
150703398 <150703398@qq.com>
2025-12-31 16:03:26 +0800

    Update packingWorker.js

--- packingWorker.js
+++ packingWorker.js
@@ -41,28 +41,24 @@ function packContainer(container,items){
 
 self.onmessage=function(e){
     const {packingData}=e.data;
-    const {containers, items, drags}=packingData;
+    const {container, items, drags}=packingData;
     let allPlacements=[];
 
-    // 装拖挂
+    // 先装拖挂
     drags.forEach(d=>{
         d.items.forEach(item=>item.isDrag=true);
         const {placements}=packContainer(d,d.items);
         allPlacements.push({container:d, items:placements});
     });
 
-    // 分配货物到车厢
-    let remainingItems=[...items];
-    containers.forEach(c=>{
-        const toPack=remainingItems.splice(0, Math.ceil(remainingItems.length/containers.length));
-        toPack.forEach(item=>item.isDrag=false);
-        const {placements}=packContainer(c,toPack);
-        allPlacements.push({container:c, items:placements});
-    });
+    // 装货物到车厢
+    items.forEach(item=>item.isDrag=false);
+    const {placements} = packContainer(container, items);
+    allPlacements.push({container:container, items:placements});
 
-    const totalVolumeUsed=allPlacements.reduce((sum,pl)=>sum+pl.items.reduce((s,i)=>s+i.width*i.height*i.depth,0),0);
-    const totalContainerVolume=allPlacements.reduce((sum,pl)=>sum+pl.container.width*pl.container.height*pl.container.depth,0);
-    const utilization = totalVolumeUsed/totalContainerVolume;
+    const totalVolumeUsed = allPlacements.reduce((sum,pl)=>sum + pl.items.reduce((s,i)=>s+i.width*i.height*i.depth,0),0);
+    const totalContainerVolume = allPlacements.reduce((sum,pl)=>sum + pl.container.width*pl.container.height*pl.container.depth,0);
+    const utilization = totalVolumeUsed / totalContainerVolume;
 
     self.postMessage({placements:allPlacements, utilization});
 }
