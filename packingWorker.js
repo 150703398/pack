17e2ef7b11ba6aa4d15a8ae55fcd0155b42ecfca
150703398 <150703398@qq.com>
2025-12-31 16:13:37 +0800

    Update packingWorker.js

--- packingWorker.js
+++ packingWorker.js
@@ -1,17 +1,9 @@
-// 简单行列装箱算法，保证可视化
+// 简单行列装箱算法保证可视化
 function packContainer(container,items){
-    let placements = [];
-    let x=0, y=0, z=0;
-    let maxRowDepth=0;
+    let placements=[], x=0, y=0, z=0, maxRowDepth=0;
     items.forEach(item=>{
-        if(x+item.width>container.width){
-            x=0;
-            z += maxRowDepth;
-            maxRowDepth=0;
-        }
-        if(z+item.depth>container.depth){
-            return; // 超出容器，放不下
-        }
+        if(x+item.width>container.width){ x=0; z+=maxRowDepth; maxRowDepth=0; }
+        if(z+item.depth>container.depth){ return; }
         placements.push({...item, position:{x,y,z}});
         x += item.width;
         if(item.depth>maxRowDepth) maxRowDepth=item.depth;
@@ -20,12 +12,12 @@ function packContainer(container,items){
     return {placements, volumeUsed};
 }
 
-self.onmessage = function(e){
+self.onmessage=function(e){
     const {packingData} = e.data;
     const {container, items, drags} = packingData;
     let allPlacements=[];
 
-    // 装拖挂
+    // 先装拖挂
     drags.forEach(d=>{
         d.items.forEach(item=>item.isDrag=true);
         const {placements} = packContainer(d,d.items);
@@ -39,7 +31,7 @@ self.onmessage = function(e){
 
     const totalVolumeUsed = allPlacements.reduce((sum,pl)=>sum + pl.items.reduce((s,i)=>s+i.width*i.height*i.depth,0),0);
     const totalContainerVolume = allPlacements.reduce((sum,pl)=>sum + pl.container.width*pl.container.height*pl.container.depth,0);
-    const utilization = totalVolumeUsed / totalContainerVolume;
+    const utilization = totalVolumeUsed/totalContainerVolume;
 
     self.postMessage({placements:allPlacements, utilization});
 }
