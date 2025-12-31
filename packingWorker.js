07eb9a338ad2c2120799780c24e70ed214f811b4
150703398 <150703398@qq.com>
2025-12-31 16:22:31 +0800

    Update packingWorker.js

--- packingWorker.js
+++ packingWorker.js
@@ -7,7 +7,8 @@ function packContainer(container,items){
         x += item.width;
         if(item.depth>maxRowDepth) maxRowDepth=item.depth;
     });
-    return {placements, volumeUsed: placements.reduce((s,i)=>s+i.width*i.height*i.depth,0)};
+    const volumeUsed = placements.reduce((s,i)=>s+i.width*i.height*i.depth,0);
+    return {placements, volumeUsed};
 }
 
 self.onmessage=function(e){
@@ -15,7 +16,7 @@ self.onmessage=function(e){
     const {container, items, drags} = packingData;
     let allPlacements=[];
 
-    // 装拖挂
+    // 拖挂先装货物
     drags.forEach(d=>{
         if(!d.items) d.items=[];
         d.items.forEach(item=>item.isDrag=true);
@@ -29,7 +30,7 @@ self.onmessage=function(e){
 
     const totalVolumeUsed = allPlacements.reduce((sum,pl)=>sum + pl.items.reduce((s,i)=>s+i.width*i.height*i.depth,0),0);
     const totalContainerVolume = allPlacements.reduce((sum,pl)=>sum + pl.container.width*pl.container.height*pl.container.depth,0);
-    const utilization = totalVolumeUsed / totalContainerVolume;
+    const utilization = totalVolumeUsed/totalContainerVolume;
 
     self.postMessage({placements:allPlacements, utilization});
 }
