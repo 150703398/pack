09465b25606eb0b5cb32ad73c13ff58c8b39decd
150703398 <150703398@qq.com>
2025-12-31 16:18:55 +0800

    Update packingWorker.js

--- packingWorker.js
+++ packingWorker.js
@@ -1,4 +1,3 @@
-// 简单行列装箱算法保证可视化
 function packContainer(container,items){
     let placements=[], x=0, y=0, z=0, maxRowDepth=0;
     items.forEach(item=>{
@@ -8,8 +7,7 @@ function packContainer(container,items){
         x += item.width;
         if(item.depth>maxRowDepth) maxRowDepth=item.depth;
     });
-    const volumeUsed = placements.reduce((s,i)=>s+i.width*i.height*i.depth,0);
-    return {placements, volumeUsed};
+    return {placements, volumeUsed: placements.reduce((s,i)=>s+i.width*i.height*i.depth,0)};
 }
 
 self.onmessage=function(e){
@@ -17,21 +15,21 @@ self.onmessage=function(e){
     const {container, items, drags} = packingData;
     let allPlacements=[];
 
-    // 先装拖挂
+    // 装拖挂
     drags.forEach(d=>{
+        if(!d.items) d.items=[];
         d.items.forEach(item=>item.isDrag=true);
         const {placements} = packContainer(d,d.items);
         allPlacements.push({container:d, items:placements});
     });
 
-    // 装货物到车厢
     items.forEach(item=>item.isDrag=false);
     const {placements} = packContainer(container, items);
     allPlacements.push({container:container, items:placements});
 
     const totalVolumeUsed = allPlacements.reduce((sum,pl)=>sum + pl.items.reduce((s,i)=>s+i.width*i.height*i.depth,0),0);
     const totalContainerVolume = allPlacements.reduce((sum,pl)=>sum + pl.container.width*pl.container.height*pl.container.depth,0);
-    const utilization = totalVolumeUsed/totalContainerVolume;
+    const utilization = totalVolumeUsed / totalContainerVolume;
 
     self.postMessage({placements:allPlacements, utilization});
 }
