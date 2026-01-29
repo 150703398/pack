e9ac0c6932c7df626aae7838bfb04919b46b91f4
150703398 <150703398@qq.com>
2026-01-29 11:40:41 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+
+class Cargo(BaseModel):
+    id: str
+    length: float   # cm
+    width: float
+    height: float
+    weight: float   # kg
+    fragile: bool = False
+    stackable: bool = True
+    max_load: float = 999999  # kg
+
+class Container(BaseModel):
+    length: float
+    width: float
+    height: float
+
+class PackRequest(BaseModel):
+    container: Container
+    cargos: List[Cargo]
