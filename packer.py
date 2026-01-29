c2713752e017dc5281b725999a6417470bb26983
150703398 <150703398@qq.com>
2026-01-29 11:41:57 +0800

    Create packer.py

---
+++ packer.py
@@ -0,0 +1,46 @@
+def pack(container, cargos):
+    # 工业排序规则
+    cargos = sorted(
+        cargos,
+        key=lambda c: (
+            -c.weight,
+            c.fragile,
+            -int(c.stackable)
+        )
+    )
+
+    placements = []
+
+    x = y = z = 0
+    layer_height = 0
+
+    for c in cargos:
+        if x + c.length > container.length:
+            x = 0
+            z += c.width
+            layer_height = 0
+
+        if z + c.width > container.width:
+            z = 0
+            y += layer_height
+            layer_height = 0
+
+        if y + c.height > container.height:
+            continue
+
+        placements.append({
+            "id": c.id,
+            "x": x,
+            "y": y,
+            "z": z,
+            "length": c.length,
+            "width": c.width,
+            "height": c.height,
+            "weight": c.weight,
+            "fragile": c.fragile
+        })
+
+        x += c.length
+        layer_height = max(layer_height, c.height)
+
+    return placements
